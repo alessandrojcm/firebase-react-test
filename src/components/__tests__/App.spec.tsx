26b4cfceaa736d61fec1685a32ef0b67c58abfe9
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-02-29 02:21:51 -0400

    feat: remove unnecessary component

--- src/components/__tests__/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-
-import { App } from '../App';
-
-test('List shows no elements message', async () => {
-  const { getByText } = render(<App />);
-  expect(getByText('No messages received')).toBeInTheDocument();
-});
