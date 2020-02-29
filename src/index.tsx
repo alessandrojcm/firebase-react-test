724da25861f6ba894b3843cb2a294f4ff99cb495
Alessandro Cuppari <alessandrojcuppari@gmail.com>
2020-02-28 23:22:59 -0400

    feat: pwa basic config

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,19 @@ import 'bulma/css/bulma.min.css';
 
 import { App } from './components/App';
 
+if (process.env.NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('./service-worker.js')
+        .then(registration => {
+          console.log('[SW] service Worker is registered', registration.scope);
+        })
+        .catch(err => {
+          console.error('[SW] service Worker registration failed:', err);
+        });
+    });
+  }
+}
+
 render(<App />, document.getElementById('root'));
